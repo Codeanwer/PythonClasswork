36f526ec969a90070834d8b32180f197622e05a9
Codeanwer <129017725+Codeanwer@users.noreply.github.com>
2023-04-12 23:33:16 +0800

    Add files via upload

---
+++ firstPythonwork.py
@@ -0,0 +1,18 @@
+items_menu = {'apple': 9.4, 'pear': 5.5, 'tomato': 1.9, 'mango': 10.5, 'watermelon': 6.2, 'Hami melon': 5.6}
+while True:
+    forms_length = input("请输入表格宽度：")
+    try:
+        forms_length = int(forms_length)
+        if forms_length > 0:
+            break
+        else:
+            print("表格宽度必须是正整数，请重新输入。")
+    except ValueError:
+        print("表格宽度必须是正整数，请重新输入。")
+print('=' * int(forms_length))
+print("Fruits" + ' ' * (int(forms_length) - 11) + "Price")  # 可跟随用户要求的表格宽度进行自定义的适应性输出
+print('-' * int(forms_length))
+for key, value in items_menu.items():
+    # 自适应输入表格宽度，-2的原因是小数点以及后一位数字占了两个字符的长度，减去即可符合格式
+    print(key, ' ' * (int(forms_length) - len(str(key)) - len(str(value)) - 2), value)
+print('=' * int(forms_length))
